0f5e027c10771e61d25e6163860adc448107830b
Hikaru Mochizuki <mcc12341on@mccnet.onmicrosoft.com>
2023-03-23 10:21:02 +0900

    edit

--- python/getresource.py
+++ python/getresource.py
@@ -3,3 +3,10 @@ import psutil
 #メモリ使用率を取得
 mem = psutil.virtual_memory()
 print(mem.percent)
+
+while True:
+    cpu = psutil.cpu_percent(interval=1)
+    print(cpu)
+
+    cpu = psutil.cpu_percent(interval=1, percpu=True)
+    print(cpu)
