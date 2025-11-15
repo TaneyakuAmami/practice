c1dc51193aafae1e091160717b043f0b24615f31
TaneyakuAmami <taneyaku.amami@gmail.com>
2025-11-16 03:46:03 +0900

    create geme folder

---
+++ python/Class/game/Creature.py
@@ -0,0 +1,8 @@
+class Creature():
+    def __init__()
+        self.hp = 100
+        self.attack = 10
+        self.deffence = 10
+
+    def action():
+        print("行動します")
---
+++ python/Class/game/Main.py
@@ -0,0 +1,2 @@
+print("game start")
+
