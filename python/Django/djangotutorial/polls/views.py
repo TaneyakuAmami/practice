25270a3d15a152b2f786caa121401c7486c91a6c
TaneyakuAmami <taneyaku.amami@gmail.com>
2026-02-09 01:11:36 +0900

    Capter 5

--- python/Django/djangotutorial/polls/views.py
+++ python/Django/djangotutorial/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from .models import Question, Choice
+from django.utils import timezone
 
 # Create your views here.
 
@@ -12,24 +13,24 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published question."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published question
+           (not including those set to be published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-
-#def detail(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, "polls/detail.html", {"question":question})
-
-#def results(request, question_id):
-#    question = get_object_or_404(Question, pk=question_id)
-#    return render(request, "polls/results.html", {"question":question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -51,8 +52,3 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing with POST data.
         # This prevents data from being posted twide if a user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-#def index(request):
-#    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#    context = {"latest_question_list": latest_question_list}
-#    return render(request, "polls/index.html", context)
